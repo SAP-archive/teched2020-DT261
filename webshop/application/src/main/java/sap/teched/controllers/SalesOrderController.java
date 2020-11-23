8b54407bfacc0760ea1a6885e50fc486dbd368be
Jordan Dukadinov <jdukadinov@gmail.com>
2020-11-23 14:58:33 +0100

    Update SalesOrderController.java

--- webshop/application/src/main/java/sap/teched/controllers/SalesOrderController.java
+++ webshop/application/src/main/java/sap/teched/controllers/SalesOrderController.java
@@ -1,10 +1,20 @@
 package sap.teched.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.sap.cloud.sdk.cloudplatform.connectivity.DestinationAccessor;
+import com.sap.cloud.sdk.cloudplatform.connectivity.HttpDestination;
+import com.sap.cloud.sdk.datamodel.odata.helper.ModificationResponse;
+import com.sap.cloud.sdk.s4hana.datamodel.odata.namespaces.salesorder.SalesOrder;
+import com.sap.cloud.sdk.s4hana.datamodel.odata.namespaces.salesorder.SalesOrderItem;
+import com.sap.cloud.sdk.s4hana.datamodel.odata.services.DefaultSalesOrderService;
 
 import sap.teched.models.WebShopOrder;
 
@@ -18,4 +28,26 @@ public class SalesOrderController {
 		return ResponseEntity.created(null).build();
 	}
 
+	private SalesOrder buildSalesOrder(WebShopOrder webShopOrder, String salesOrderType, String soldToParty) {
+		List<SalesOrderItem> s4items = buildItems(webShopOrder);
+		
+		SalesOrder salesOrder = SalesOrder.builder()
+				.salesOrderType(salesOrderType)
+				.soldToParty(soldToParty)
+				.item(s4items.toArray(new SalesOrderItem[s4items.size()]))
+				.build();
+		return salesOrder;
+	}
+
+	private List<SalesOrderItem> buildItems(WebShopOrder webShopOrder) {
+		List<SalesOrderItem> s4items = new ArrayList<>();
+		for (WebShopOrder.LineItem webShopItem : webShopOrder.getLineItems()) {
+			SalesOrderItem s4Item = SalesOrderItem.builder()
+					.material(webShopItem.getProductId())
+					.requestedQuantity(webShopItem.getQuantity())
+					.build();
+			s4items.add(s4Item);
+		}
+		return s4items;
+	}
 }
